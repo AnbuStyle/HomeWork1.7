4ff6f848ac48377fb07c46cf0f40bd7734b74da9
AnbuStyle <khumaun99@gmail.com>
2021-07-18 23:28:48 +0300

    Home Work 1.8

--- src/main/java/ru/geekbrains/server/SimpleAuthService.java
+++ src/main/java/ru/geekbrains/server/SimpleAuthService.java
@@ -5,44 +5,35 @@ import java.util.List;
 
 public class SimpleAuthService implements AuthService {
 
-   private static class UserData {
-       private final String login;
-       private final String password;
-       private final String nickname;
+    private static class UserData {
+        private final String login;
+        private final String password;
+        private final String nickname;
 
-       public UserData(String login, String password, String nickname) {
-           this.login = login;
-           this.password = password;
-           this.nickname = nickname;
-       }
+        public UserData(String login, String password, String nickname) {
+            this.login = login;
+            this.password = password;
+            this.nickname = nickname;
+        }
 
-       public String getLogin() {
-           return login;
-       }
 
-       public String getPassword() {
-           return password;
-       }
+    }
 
-       public String getNickname() {
-           return nickname;
-       }
-   }
     private List<UserData> users;
 
     public SimpleAuthService() {
         users = new ArrayList<>();
+
         for (int i = 0; i < 5; i++) {
-
-
-        users.add(new UserData("login" + i, "password" + i, "nick" + i));
+            users.add(new UserData("login" + i, "pass" + i, "nick" + i));
         }
+        //users.add(new UserData("admin", "123", "Admin"));
     }
 
     @Override
-    public String getNicknameByLoginAndPassword(String login, String password){
+    public String getNicknameByLoginAndPassword(String login, String password) {
         for (UserData user : users) {
-            if(login.equals(user.login) && password.equals(user.password)) {
+            if (login.equals(user.login) && password.equals(user.password)){
                 return user.nickname;
             }
         }
